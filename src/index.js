efb998a6fc542929e0f400c29a67c52c671d52e8
Smit Patel <smitbpatel1999@gmail.com>
2020-12-04 19:44:35 -0600

    Fixing contact

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ const app = require('./app');
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   /* eslint-disable no-console */
-  console.log(`Listening: http://localhost:${port}`);
+  console.log(`Listening: http://0.0.0.0:${port}`);
   /* eslint-enable no-console */
 });
