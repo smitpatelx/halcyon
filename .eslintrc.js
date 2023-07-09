fcc2425917941d5ea3a64e49c1ae9cafac3c032f
smitpatelx <smitbpatel1999@gmail.com>
2023-07-09 03:03:24 -0500

    Eslint config fixed.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,15 @@
 module.exports = {
-    env: {
-        jest: true,
-    },
-    extends: 'airbnb-base',
-    rules: {
-        'comma-dangle': 0,
-        'no-underscore-dangle': 0,
-        'no-param-reassign': 0,
-        'no-return-assign': 0,
-        camelcase: 0,
-    }
+  env: {
+    jest: true,
+  },
+  extends: 'airbnb-base',
+  rules: {
+    'comma-dangle': 0,
+    'no-underscore-dangle': 0,
+    'no-param-reassign': 0,
+    'no-return-assign': 0,
+    'no-unused-vars': 0,
+    'new-cap': ['error', { newIsCap: false }],
+    camelcase: 0,
+  }
 };
